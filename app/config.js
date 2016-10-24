61029a4b93b46aa26b506d918a83704fa4b959ba
GeeKaven <xiaokang_show@live.com>
2016-10-25 01:30:10 +0800

    remove compile file

--- app/config.js
+++
@@ -1,3 +0,0 @@
-"use strict";exports.__esModule = true;exports.default = {
-  port: 3000 };module.exports = exports['default'];
-//# sourceMappingURL=config.js.map
--- app/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";exports.__esModule = true;
-
-var _app = require("./app");var _app2 = _interopRequireDefault(_app);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}require("babel-runtime/core-js/promise").default = require("bluebird");global.Promise = require("bluebird");exports.default = _app2.default;module.exports = exports['default'];
-//# sourceMappingURL=index.js.map
