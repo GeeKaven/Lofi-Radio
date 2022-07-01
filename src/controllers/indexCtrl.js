cdcd6388189faca791e09b5da17cf1e7a83948ca
GeeKaven <i@tawawa.moe>
2022-07-01 23:09:36 +0800

    use vite

--- src/controllers/indexCtrl.js
+++
@@ -1,8 +0,0 @@
-
-export default async (ctx, next) => {
-
-  await ctx.render('index', {
-      title : "NekoFM",
-      desc : "Music change the world"
-  })
-}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-require("babel-runtime/core-js/promise").default = require("bluebird");
-global.Promise = require("bluebird");
-import app from './app'
-export default app
