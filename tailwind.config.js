c5ec4cfe76d4931a935b9269a825292f226dd8f1
GeeKaven <i@tawawa.moe>
2022-10-25 11:52:51 +0800

    change

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{jsx,js,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
